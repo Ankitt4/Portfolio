ad2b71f83d5b2342e66e44a745599790c5b1b85d
Ankitt4 <115177813+Ankitt4@users.noreply.github.com>
2024-08-04 23:14:24 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navabr');
+let navbar = document.querySelector('.navbar');
 
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('fa-xmark');
